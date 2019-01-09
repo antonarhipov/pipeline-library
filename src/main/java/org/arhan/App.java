233b62618e3d7ec917d6df4f7692ddff006d8ef8
Anton Arhipov <ant.arhipov@gmail.com>
2019-01-09 19:52:14 +0100

    initial commit

---
+++ src/main/java/org/arhan/App.java
@@ -0,0 +1,11 @@
+package org.arhan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void hello(){
+    }
+}
---
+++ src/test/java/org/arhan/LibTest.java
@@ -0,0 +1,15 @@
+package org.arhan;
+
+import junit.framework.TestCase;
+import junit.framework.TestCase;
+
+public class LibTest extends TestCase {
+    public void testApp() {
+        assertTrue( true );
+    }
+
+    public void testApp3() {
+        System.out.println("LibTest.testApp3");
+        assertEquals("The values should be equal", 1, 1);
+    }
+}
